c6f4e10a63e022d2f6d1f911a2e54a7c531f14cb
Uğur <62259649+ugurdikici@users.noreply.github.com>
2020-07-12 22:07:43 +0300

    Initial commit

---
+++ Parola_Üretici.py
@@ -0,0 +1,39 @@
+﻿from tkinter import *
+from random import sample
+from tkinter import messagebox as msgbx
+
+root = Tk()
+root.geometry("970x570")
+root.title("Parola Üret")
+root.resizable(0, 0)
+root.config(bg="black")
+Label(root, text="Parola Üreticisi", font=("Times", 35, "bold underline"), bg="black", fg="yellow").pack()
+Label(root, text="Parolanız Kaç Basamaklı: ", font=("Times", 35, "bold"), bg="black", fg="white").place(x=70, y=110)
+length_entry = Entry(root, font=("Helvetica", 25))
+length_entry.place(width=300, height=50, x=610, y=115)
+text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ÜĞÇüğç@#$&()<>{[}]/-+é!'^+%&?"
+
+
+def getpasswd():
+    try:
+        passwd = "".join(sample(text, int(length_entry.get())))
+        passwd_entry.delete(0, END)
+        passwd_entry.insert(END, passwd)
+    except ValueError or UnboundLocalError:
+        msgbx.showinfo("Error", "Lütfen 1 ile 77 Arasında Bir Sayı Giriniz")
+
+
+Button(root, text="Şifreniz", bd=10, bg="grey", fg="black", command=getpasswd, font=("Times", 25, "bold")).place(x=290, y=200, height=80, width=320)
+Label(root, text="Şifre: ", font=("Times", 35, "bold"), bg="black", fg="white").place(x=210, y=305)
+passwd_entry = Entry(root, font=("Times", 35, "bold"))
+passwd_entry.place(x=430, y=310)
+
+
+def quitted():
+    msgbx.showinfo("Thanks", "Uğur Dikici")
+    quit()
+
+
+Button(root, text="ÇIKIŞ!!", bd=10, bg="grey", fg="black", command=quitted, font=("Times", 15, "bold")).place(x=370, y=380)
+Label(root, text="©UĞURDİKİCİ", bg="black", fg="white", font=("Helvetica", 12)).pack(side=BOTTOM, anchor=SE)
+root.mainloop()
---
+++ İP_Bul.py
@@ -0,0 +1,35 @@
+﻿from tkinter import *
+from socket import gethostname, gethostbyname
+from tkinter import messagebox as msgbx
+
+root = Tk()
+root.geometry("900x500")
+root.title("IP Adres")
+root.config(bg="black")
+Label(root, text="Ip Bulucu", font=("Times", 35, "bold underline"), bg="black", fg="grey").pack()
+
+
+def getIP():
+    hostname = gethostname()
+    ip = gethostbyname(hostname)
+    giveip.delete(0, END)
+    if ip == "127.0.0.1":
+        msgbx.showinfo("İnternet YOK", "İnternete Bağlı Değilsiniz\nYerel IP Adresiniz Gösterilecek")
+        giveip.insert(END, ip)
+    else:
+        giveip.insert(END, ip)
+
+
+def quitted():
+    msgbx.showinfo("UğurDikici")
+    quit()
+
+    
+
+
+Button(root, text="Get IP Address", font=("Times", 25, "bold underline"), bg="black", fg="grey", bd=10, command=getIP).place(x=140, y=100, width=270, height=70)
+giveip = Entry(root, bd=10, font=("Times", 25, "bold"))
+giveip.place(x=450, y=100, width=240, height=65)
+Button(root, text="Çıkış", font=("Times", 25, "bold underline"), bg="black", fg="grey", bd=10, command=quitted).place(x=750, y=420)
+Label(root, text="UğurDikici", font=("Times", 25, "bold"), bg="black", fg="grey", bd=10).pack(side=BOTTOM, anchor=W)
+root.mainloop()
